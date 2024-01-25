79da06b06497db4e2c4302b341c833829a05381a
Aapo Jutila <aapo.jutila01@gmail.com>
2024-01-25 02:25:30 +0200

    apufunktioita ja yksikkötestejä step4-5

--- tests/lists.test.js
+++ tests/lists.test.js
@@ -14,7 +14,7 @@ describe('total likes', () => {
       title: "React patterns",
       author: "Michael Chan",
       url: "https://reactpatterns.com/",
-      likes: 7,
+      likes: 77,
       __v: 0
     },
     {
@@ -59,21 +59,43 @@ describe('total likes', () => {
     }  
   ]
 
+  /*
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(blogs)
     expect(result).toBe(36)
   })
+  */
 
   test("find a correct blog that has the most likes", () => {
     const result = listHelper.favoriteBlogs(blogs)
     expect(result).toEqual(
       {
-        _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12,
+        _id: "5a422a851b54a676234d17f7",
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 77,
         __v: 0
+      }
+    )
+  })
+
+  test("most blogs is calculated correctly", () => {
+    const result = listHelper.mostBlogs(blogs)
+    expect(result).toEqual(
+      {
+        author: "Robert C. Martin",
+        blogs: 3
+      }
+    )
+  })
+
+  test("most likes is calculated correctly", () => {
+    const result = listHelper.mostLikes(blogs)
+    expect(result).toEqual(
+      {
+        author: "Michael Chan",
+        likes: 77
       }
     )
   })
